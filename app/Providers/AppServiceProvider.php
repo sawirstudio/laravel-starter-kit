822270f4c29c682a67b6b2740426d80dcc2f4fc8
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2026-09-14 10:21:20 +0700

    app: better defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,17 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Sleep;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +29,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
+        DB::prohibitDestructiveCommands(app()->isProduction());
+        Date::use(CarbonImmutable::class);
+        Vite::useAggressivePrefetching();
+        Http::preventStrayRequests();
+        Sleep::fake();
+        URL::forceHttps();
+        Password::defaults(fn (): ?Password => app()->isProduction() ? Password::min(12)->letters()->mixedCase()->numbers()->symbols()->max(255)->uncompromised() : null);
+
+        Relation::enforceMorphMap([
+            'user' => \App\Models\User::class,
+        ]);
     }
 }
